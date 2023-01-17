32d18c28b28ea964f5ef0bb3e9b0a5cbe8d2ca19
JDevelops <joseprz340@gmail.com>
2023-01-16 19:16:02 -0600

    Adding files

---
+++ src/Component/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({ name, email, id }) => {
+  return (
+    <div className='tc white grow bg-dark-gray br3 pa4 ma4 dib bw2 shadow-5 w-20'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
